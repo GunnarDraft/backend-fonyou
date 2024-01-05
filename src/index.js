9190fcefb661a481f7867fea6f6635a16ce9618d
gunnardraft <gunnardraft@live.com.mx>
2024-01-04 21:34:57 -0600

    backend service

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const axios = require('axios')
+require('dotenv').config()
+var cors = require('cors')
+
+const apiUrl = process.env.API_URL
+
+var whitelist = ['http://localhost:3000','https://gunnar-fonyou-demo.vercel.app']
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get('/character', cors(corsOptions), async (req, res) => {
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        page: req.query.page || 1,
+        name: req.query.name || '',
+      },
+    });
+
+    res.json(response.data); 
+  } catch (error) {
+    console.error('error',error);
+    res.status(500).json({ error: 'Error al obtener datos de la API externa' });
+  }
+});
+app.listen(port, () => {
+  console.log("My port: " + port);
+});
